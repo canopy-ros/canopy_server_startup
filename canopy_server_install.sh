771d5ff2e48eac4581facb31dccc658903019408
Raphael Chang <raphaelchang@mit.edu>
2016-11-21 17:05:50 -0500

    Update canopy_server_install.sh

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
-wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 sudo apt-get install curl -y
 sudo apt-get install redis-server -y
 sudo apt-get install docker.io -y
@@ -35,6 +35,7 @@ git clone https://github.com/canopy-ros/canopy_server_paas
 cd canopy_server_paas
 go install
 cd docker
+sudo docker -d
 sudo docker build --tag="canopy" .
 mkdir -p ~/meteor
 cd ~/meteor
