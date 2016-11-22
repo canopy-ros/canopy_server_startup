024c096451276210dc01684c34a6edf93aeac62f
Alexander Wallar <alex@wallar.me>
2016-11-22 15:37:44 -0500

    Travis hopefully work

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
