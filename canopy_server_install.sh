f968dff515ba5e5c1ade61b06efd59773724fa59
Alexander Wallar <alex@wallar.me>
2016-11-22 15:43:05 -0500

    will docker work now?

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -5,7 +5,10 @@ wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 sudo apt-get install curl -y
 sudo apt-get install redis-server -y
-sudo apt-get install docker.io -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o \
+    Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confnew" install docker.io
+# sudo apt-get install docker.io -y
 sudo apt-get install python-pip -y
 curl https://install.meteor.com/ | sh
 cd ~
