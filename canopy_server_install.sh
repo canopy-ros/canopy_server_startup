8869b51cb3ed444ff352d67ab800663e800289c0
Raphael Chang <raphaelchang@mit.edu>
2016-11-23 17:04:18 -0500

    which go

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -24,6 +24,7 @@ mkdir -p go/bin
 mkdir -p go/pkg
 cd go/src/github.com/canopy-ros
 go version
+which go
 go get github.com/gorilla/websocket
 go get github.com/garyburd/redigo/redis
 go get github.com/docker/engine-api
