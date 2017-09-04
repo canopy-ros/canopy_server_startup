0b37e5296447610849c82e2f266f4aa6f650be93
Roomba 0 <alex@wallar.me>
2017-09-03 21:08:58 -0400

    add simple install

--- canopy_server_comm_install.sh
+++ canopy_server_comm_install.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
-set -e
+#set -e
 
 sudo apt-get update
-GIMME_OUTPUT=$(gimme 1.7.3) && eval "$GIMME_OUTPUT"
-wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 sudo apt-get install curl -y
 sudo apt-get install redis-server -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o \
-    Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confnew" install docker-engine
-# sudo apt-get install docker.io -y
 sudo apt-get install python-pip -y
 curl https://install.meteor.com/ | sh
 cd ~
-#echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-#echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-#echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 export GOPATH=$(pwd)/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$PATH:/usr/local/go/bin
@@ -39,12 +29,6 @@ go get gopkg.in/mgo.v2
 git clone https://github.com/canopy-ros/canopy_server_comm
 cd canopy_server_comm
 go install
-cd ..
-git clone https://github.com/canopy-ros/canopy_server_paas
-cd canopy_server_paas
-go install
-cd docker
-sudo docker build --tag="canopy" .
 mkdir -p ~/meteor
 cd ~/meteor
 git clone https://github.com/canopy-ros/canopy_server_dashboard
