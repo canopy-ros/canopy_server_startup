89b45c99401c717ba6d671f114a4c7db24595502
Alexander Wallar <alex@wallar.me>
2017-09-03 22:10:28 -0400

    add script to install comm server

--- canopy_server_comm_install.sh
+++ canopy_server_comm_install.sh
@@ -2,33 +2,23 @@
 #set -e
 
 sudo apt-get update
+wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
+mkdir -p ws/src/github.com/canopy-ros
+mkdir -p ws/bin
+mkdir -p ws/pkg
+sudo tar -xzf go1.7.3.linux-amd64.tar.gz
 sudo apt-get install curl -y
 sudo apt-get install redis-server -y
 sudo apt-get install python-pip -y
-curl https://install.meteor.com/ | sh
-cd ~
-export GOPATH=$(pwd)/go
+export GOPATH=$(pwd)/ws
 export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$PATH:/usr/local/go/bin
-mkdir -p go/src/github.com/canopy-ros
-mkdir -p go/bin
-mkdir -p go/pkg
-cd go/src/github.com/canopy-ros
-go version
-go get github.com/gorilla/websocket
-go get github.com/garyburd/redigo/redis
-go get github.com/docker/engine-api
-go get github.com/docker/go-connections
-go get github.com/docker/go-units
-go get github.com/docker/distribution
+export PATH=$PATH:$(pwd)/go/bin
+cd ws/src/github.com/canopy-ros
 go get github.com/Sirupsen/logrus
 go get golang.org/x/net/context
+go get github.com/gorilla/websocket
+go get github.com/garyburd/redigo/redis
 go get golang.org/x/net/proxy
-go get github.com/opencontainers/runc || true
-go get gopkg.in/mgo.v2
 git clone https://github.com/canopy-ros/canopy_server_comm
 cd canopy_server_comm
 go install
-mkdir -p ~/meteor
-cd ~/meteor
-git clone https://github.com/canopy-ros/canopy_server_dashboard
