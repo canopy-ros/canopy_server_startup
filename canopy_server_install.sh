ee71d2aa4f942849c5c3a3c36617283b4dcde648
Zareen Choudhury <zareenc@mit.edu>
2017-09-29 12:57:59 -0400

    Fix docker_ce installation for Ubuntu 16.04

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -8,10 +8,13 @@ wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 sudo apt-get install curl -y
 sudo apt-get install redis-server -y
+sudo apt-get install apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o \
     Dpkg::Options::="--force-confdef" \
     -o Dpkg::Options::="--force-confnew" install docker-ce
-# sudo apt-get install docker.io -y
 sudo apt-get install python-pip -y
 curl https://install.meteor.com/ | sh
 cd ~
