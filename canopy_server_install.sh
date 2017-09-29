cb8d42b76c2d6e71b400fa384a2d64a31e09330f
Zareen Choudhury <zareenc@mit.edu>
2017-09-29 15:24:17 -0400

    Replace reference to specific Ubuntu version in Docker installation

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -10,7 +10,7 @@ sudo apt-get install curl -y
 sudo apt-get install redis-server -y
 sudo apt-get install apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o \
     Dpkg::Options::="--force-confdef" \
