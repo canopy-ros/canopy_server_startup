5508375e5819540573e550239d58a9c0f5d239a6
Raphael Chang <raphaelchang@mit.edu>
2017-10-15 18:37:13 -0400

    Update canopy_server_start.sh

--- canopy_server_start.sh
+++ canopy_server_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 redis-server &
-service start canopy_server_comm.service
-service start canopy_server_paas.service
+service canopy_server_comm.service start
+service canopy_server_paas.service start
 cd ~/meteor/canopy_server_dashboard
 (env REDIS_CONFIGURE_KEYSPACE_NOTIFICATIONS=1 meteor) &
 #sudo chmod 777 /var/run/docker.sock
