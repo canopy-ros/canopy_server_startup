71368d63c828fff9b334485c266211d615a8d364
Zareen Choudhury <zareenc@mit.edu>
2017-10-17 18:06:14 -0400

    Start systemd service with sudo

--- canopy_server_start.sh
+++ canopy_server_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 redis-server &
-service canopy_server_comm.service start
-service canopy_server_paas.service start
+sudo service canopy_server_comm start
+sudo service canopy_server_paas start
 cd ~/meteor/canopy_server_dashboard
 (env REDIS_CONFIGURE_KEYSPACE_NOTIFICATIONS=1 meteor) &
 #sudo chmod 777 /var/run/docker.sock
