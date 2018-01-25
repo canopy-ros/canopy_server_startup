595ac21452f0011fd07fe156b4f574de05bb91bb
Zareen Choudhury <zareenc@mit.edu>
2018-01-25 13:44:46 -0500

    Delete redundant line

--- canopy_server_install.sh
+++ canopy_server_install.sh
@@ -28,7 +28,6 @@ while true; do
     esac
 done
 
-export CANOPY_DIR=$(pwd)
 sudo apt-get update
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
